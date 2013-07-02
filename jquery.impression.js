4a6040882597a930827b8eeaaa7d85eb11a9b805
dirkraft <dirkraft@gmail.com>
2013-07-02 17:52:15 -0500

    test.html page

---
+++ jquery.impression.js
@@ -0,0 +1,21 @@
+/** turn a form into a js object */
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+$.fn.impression = function() {
+    return this;
+};
